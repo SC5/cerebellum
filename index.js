62b6f9ba0d44d889e72cc42b5fcebef6d708db15
Lari Hoppula <lari@sc5.io>
2014-10-03 12:38:29 +0300

    Replaced collections option with stores, added support for models
    - Client now supports options.passthrough, which is array of urls.
    Passthrough urls will skip HTML5 router, useful for implementing
    server side only routes like /login & /logout.
    
    - Pass middleware to server as options.middleware instead of
    options.middlewares.
    
    - Server now supports passing settings to
    express app, use options.app.
    
    - Wrapped express.static registration
    to function, so you can call it your app's server.js after defining
    your routes.
    
    - Optimized cloning of index.html's content in server's render action.
    
    - Better error messages for server's render action
    
    - Store now supports passing cookie as option, you can use it to pass
    session id for server side API calls.
    
    - Store is now Exoskeleton.Events enabled, meaning it can act
    as event bus for all stores.
    
    - Store's get is now known as fetch, get is used for returning
    reference to that particular store.
    
    - Store's fetch now resolves even with error status, so API errors
    don't prevent render action from completing on server.

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function validateOptions(options) {
     throw new Error("You must define routes option or your app won't respond to anything");
   }
 
-  if (!options.collections || typeof options.collections !== "object") {
-    console.warn("You won't be able to use this.store in router without defining any collections");
+  if (!options.stores || typeof options.stores !== "object") {
+    console.warn("You won't be able to use this.store in router without defining any stores");
   }
 }
 
