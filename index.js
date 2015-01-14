55858e3b534dae83a842b1339f11bc9c1f789dea
Lari Hoppula <lari@sc5.io>
2015-01-14 14:07:12 +0200

    Added routeContext parameter for client & server constructors
    You can define custom 'this' context for route handlers with
    routeContext parameter.
    
    Also did some code cleanup and moved constructor options validation to
    separate module.

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
 var exoskeleton = require("./lib/wrapper/exoskeleton");
 
-function validateOptions(options) {
-  if (!options.storeId || typeof options.storeId !== "string") {
-    throw new Error("You must define storeId option, it's used for collections cache, e.g. 'store'");
-  }
-
-  if (!options.render || typeof options.render !== "function") {
-    throw new Error("You must define render option, it will be called when route handler finishes.");
-  }
-
-  if (!options.routes || typeof options.routes !== "object") {
-    throw new Error("You must define routes option or your app won't respond to anything");
-  }
-
-  if (!options.stores || typeof options.stores !== "object") {
-    console.warn("You won't be able to use this.store in router without defining any stores");
-  }
-}
-
-var cerebellum = {
-  server: function(options) {
-    validateOptions(options);
-    return require("./lib/server")(options);
-  },
-  client: function(options) {
-    validateOptions(options);
-    return require("./lib/client")(options);
-  },
-  Collection: exoskeleton.Collection,
-  Model: exoskeleton.Model
+module.exports.server = function server(options, routeContext) {
+  return require("./lib/server")(options, routeContext);
 };
 
-module.exports = cerebellum;
+module.exports.client = function(options, routeContext) {
+  return require("./lib/client")(options, routeContext);
+};
+
+module.exports.Collection = exoskeleton.Collection;
+module.exports.Model = exoskeleton.Model;
