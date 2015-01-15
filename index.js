eb0e81f14fe4023511b6b375c8a1810351d3762a
Lari Hoppula <lari@sc5.io>
2015-01-15 11:50:01 +0200

    Export Store & DOMReady

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ module.exports.client = function(options, routeContext) {
   return require("./lib/client")(options, routeContext);
 };
 
+module.exports.Store = require('./lib/store');
 module.exports.Collection = exoskeleton.Collection;
 module.exports.Model = exoskeleton.Model;
+module.exports.DOMReady = function() {
+  return require('./lib/domready');
+}
