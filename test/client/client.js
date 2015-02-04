a29ccd4b154f05b2c098ee0ecd4fef845f4afb8b
Lari Hoppula <lari@sc5.io>
2015-02-04 12:24:59 +0200

    Client now emits an event after route handler has rendered the route
    Client extends Vertebrae's Events and returns an object with methods
    from Events. Currently only event that's triggered is "render" after
    route rendering finishes.
    
    Client now calls the initialize callback only after DOM is ready.
    
    Also added lots of new client-side tests.

--- test/client/client.js
+++ test/client/client.js
@@ -1,6 +1,7 @@
 (function() {
 
   var cerebellum = window.Cerebellum;
+  var Collection = cerebellum.Collection;
   var appId = "app";
   var storeId = "store_state_from_server";
   var appContainer = document.getElementById(appId);
@@ -21,9 +22,25 @@
             return new Promise(function(resolve, reject) {
               resolve({value: "index content"});
             });
+          },
+          "/second_route": function() {
+            return {value: "second view"};
+          },
+          "/third_route/:category_id/:id": function(categoryId, id) {
+            return {value: [categoryId, id]};
+          },
+          "/fourth/:optional?": function(optional) {
+            return {value: optional};
+          },
+          "/movies": function() {
+            return this.store.fetch("movies").then(function(movies) {
+              return {value: movies.map(function(movie) { return movie.name; }).join(",")};
+            });
           }
         },
-        stores: {}
+        stores: {
+          movies: Collection.extend({})
+        }
       };
     });
 
@@ -32,15 +49,96 @@
     });
 
     it('should render index route handler\'s response to #app after initialization', function(done) {
-      options.initialize = function(client) {
-        // TODO: better way to ensure this runs after render has finished
-        setTimeout(function() {
-          appContainer.innerHTML.should.equal("index content");
-           done();
-        }, 10);
-      };
-      cerebellum.client(options);
+      var clientEvents = cerebellum.client(options);
+      clientEvents.on("render", function(route) {
+        route.should.equal("/");
+        appContainer.innerHTML.should.equal("index content");
+        clientEvents.off();
+        done();
+      });
     });
+
+    it('should route to another url and render it\'s content', function(done) {
+      options.initialize = function(client) {
+        client.router.show("/second_route");
+      };
+
+      var clientEvents = cerebellum.client(options);
+
+      clientEvents.on("render", function(route) {
+        route.should.equal("/second_route");
+        appContainer.innerHTML.should.equal("second view");
+        clientEvents.off();
+        done();
+      });
+
+    });
+
+    it('should handle route parameters', function(done) {
+      options.initialize = function(client) {
+        client.router.show("/third_route/1/2");
+      };
+
+      var clientEvents = cerebellum.client(options);
+
+      clientEvents.on("render", function(route) {
+        route.should.equal("/third_route/:category_id/:id");
+        appContainer.innerHTML.should.equal("1,2");
+        clientEvents.off();
+        done();
+      });
+
+    });
+
+    it('should render view without optional route param', function(done) {
+      options.initialize = function(client) {
+        client.router.show("/fourth");
+      };
+
+      var clientEvents = cerebellum.client(options);
+
+      clientEvents.on("render", function(route) {
+        route.should.equal("/fourth/:optional?");
+        appContainer.innerHTML.should.equal("undefined");
+        clientEvents.off();
+        done();
+      });
+
+    });
+
+    it('should render view with optional route param', function(done) {
+      options.initialize = function(client) {
+        client.router.show("/fourth/123");
+      };
+
+      var clientEvents = cerebellum.client(options);
+
+      clientEvents.on("render", function(route) {
+        route.should.equal("/fourth/:optional?");
+        appContainer.innerHTML.should.equal("123");
+        clientEvents.off();
+        done();
+      });
+
+    });
+
+    it('should bootstrap from initial JSON', function(done) {
+      options.initialize = function(client) {
+        client.router.show("/movies");
+      };
+
+      var clientEvents = cerebellum.client(options);
+
+      clientEvents.on("render", function(route) {
+        route.should.equal("/movies");
+        appContainer.innerHTML.should.equal("Interstellar,Inception,Insomnia");
+        clientEvents.off();
+        done();
+      });
+
+    });
+
+
 
   });
 
