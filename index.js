4db0af820440d1ddb69ff85cd06751ae0ccaeb10
Lari Hoppula <lari@hoppula.com>
2015-04-17 01:48:26 +0300

    Require from ./lib in index.js

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ var Model = require('vertebrae/model')({sync: Sync});
 var Collection = require('vertebrae/collection')({sync: Sync}, Model);
 
 module.exports.server = function server(options, routeContext) {
-  return require("./server")(options, routeContext);
+  return require("./lib/server")(options, routeContext);
 };
 
 module.exports.client = function(options, routeContext) {
-  return require("./client")(options, routeContext);
+  return require("./lib/client")(options, routeContext);
 };
 
 module.exports.Store = require('./store');
 module.exports.Collection = Collection;
 module.exports.Model = Model;
 module.exports.DOMReady = function() {
-  return require('./domready');
+  return require('./lib/domready');
 }
